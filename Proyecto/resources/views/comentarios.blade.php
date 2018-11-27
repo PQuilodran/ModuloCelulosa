9a4f96683ff1ca96e887b596451a5f93a19131a7
basty <bmignolet2016@alu.uct.cl>
2018-11-27 11:22:08 -0300

    haciendos pruebas

---
+++ Proyecto/resources/views/comentarios.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>comen</title>
+</head>
+<body>
+	<h1>Estos son los comentarios</h1>
+</body>
+</html>
--- Proyecto/routes/web.php
+++ Proyecto/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/coment', function () {
+    return view('comentarios');
+});
