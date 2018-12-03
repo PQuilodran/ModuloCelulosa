78d14b8bde105f9f595a6d4a9334e4e9ef79b0f9
basty <bmignolet2016@alu.uct.cl>
2018-12-02 23:13:34 -0300

    comentarios en el show

--- Proyecto/resources/views/Bitacora/show.blade.php
+++ Proyecto/resources/views/Bitacora/show.blade.php
@@ -28,4 +28,26 @@
             </div>
         </div>
     </div>
+
+    
+    <div class="col-md-10 clear"><hr><h3>Comentario</h3>
+            </div>
+            
+                                      
+           <div class="col-md-10">
+                        
+                  <div style="background-color:rgba(153, 243, 243,0.5); margin-top: 2%">
+                      
+                  
+                                     
+                   </div>                      
+                                
+                        
+              <form class="form-group" action="guardar.php" method='POST'>
+                  <label for="comennt">Agregar comentario</label>
+                  <textarea type="text" rows="5" name="coment" id="coment" class="form-control"></textarea>
+                   <button type="submit" class="btn btn-primary ">Enviar Comentario</button>
+               </form>
+        </div>
+
 @endsection
